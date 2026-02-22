cd4d685664005fc139145f7b7ed2fd793d296a95
InternetMadeCoder <anonymous210705@gmail.com>
2026-02-22 13:50:12 +0530

    feat(api): add CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from search import search
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
